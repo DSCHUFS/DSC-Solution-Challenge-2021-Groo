364558fcc45bc1f980acc902b6af3f27513a43bc
DevLuce <whchoi000@gmail.com>
2021-02-21 14:45:35 +0900

    Modify app bar to Home Screen

--- groo/lib/screens/home_screen.dart
+++ groo/lib/screens/home_screen.dart
@@ -3,6 +3,15 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Groo"),
+        centerTitle: false,
+        actions: [
+          IconButton(icon: Icon(Icons.favorite_border), onPressed: null),
+          IconButton(icon: Icon(Icons.settings), onPressed: null),
+        ],
+      ),
+    );
   }
 }

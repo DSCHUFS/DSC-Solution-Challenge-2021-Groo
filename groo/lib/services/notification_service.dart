4e55bbf6c436b7cdf36d7cb786949db57d8d763f
DevLuce <whchoi000@gmail.com>
2021-03-19 23:06:39 +0900

    Add Notification Service

---
+++ groo/lib/services/notification_service.dart
@@ -0,0 +1,108 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class NotificationService {
+  String _timezone = 'Unknown';
+  List<String> _availableTimezones = <String>[];
+  bool isNotify = false;
+
+  NotificationService() {
+    _initData();
+    _loadPref();
+  }
+
+  Future<void> _initData() async {
+    try {
+      _timezone = await FlutterNativeTimezone.getLocalTimezone();
+    } catch (e) {
+      print('Could not get the local timezone');
+    }
+    try {
+      _availableTimezones = await FlutterNativeTimezone.getAvailableTimezones();
+    } catch (e) {
+      print('Could not get available timezones');
+    }
+  }
+
+  Future<void> _loadPref() async {
+    final prefs = await SharedPreferences.getInstance();
+    isNotify = (prefs.getBool('isNotify') ?? false);
+  }
+
+  Future<void> _setPref(bool value) async {
+    final prefs = await SharedPreferences.getInstance();
+    isNotify = value;
+    prefs.setBool('isNotify', value);
+  }
+
+  Future<void> showNotification() async {
+    final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    final result = await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+    var androidDetails = new AndroidNotificationDetails(
+      "channelId",
+      "channelName",
+      "channelDescription",
+      importance: Importance.max,
+      priority: Priority.max,
+    );
+    var iosDetails = new IOSNotificationDetails();
+    var generalNotificationDetails =
+        new NotificationDetails(android: androidDetails, iOS: iosDetails);
+
+    if (result) {
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>()
+          ?.deleteNotificationChannelGroup('id');
+
+      await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        "notiTitle",
+        "notiDesc",
+        _setNotiTime(),
+        generalNotificationDetails,
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time,
+      );
+
+      _setPref(true);
+    }
+  }
+
+  Future<void> cancelAllNotification() async {
+    final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    await flutterLocalNotificationsPlugin.cancelAll();
+    _setPref(false);
+  }
+
+  tz.TZDateTime _setNotiTime() {
+    tz.initializeTimeZones();
+    tz.setLocalLocation(tz.getLocation(_timezone));
+
+    final now = tz.TZDateTime.now(tz.local);
+    var scheduledDate = tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      20,
+      5,
+    );
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(const Duration(days: 1));
+    }
+    return scheduledDate;
+  }
+}

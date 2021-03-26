f45c4f37656b1b54f58fd2da78fcfd6506767c76
DevLuce <whchoi000@gmail.com>
2021-03-26 17:48:25 +0900

    Delete all MaterialApp Class

--- groo/lib/screens/attend_screen.dart
+++ groo/lib/screens/attend_screen.dart
@@ -18,14 +18,25 @@ class _AttendScreenState extends State<AttendScreen> {
   final int todayIndex = 0;
   final int challengeDays = 20;
   List<bool> attendList = List.filled(20, false);
-  List<bool> attendList2 = List.generate(20, (index) {});
-  List<Icon> iconList = List.filled(
+  List<bool> attendList2 = List.generate(20, (index) {
+    if (index < 5) {
+      return true;
+    } else
+      return false;
+  });
+  List<Icon> iconList = List.generate(
       20,
-      Icon(
-        FontAwesomeIcons.mehBlank,
-        size: 55,
-        color: Colors.blueGrey[900],
-      ));
+      (index) => index < 5
+          ? Icon(
+              FontAwesomeIcons.laugh,
+              size: 55,
+              color: Colors.blueGrey[900],
+            )
+          : Icon(
+              FontAwesomeIcons.mehBlank,
+              size: 55,
+              color: Colors.blueGrey[900],
+            ));
 
   Function checkList(int index) {
     return () {
@@ -49,55 +60,53 @@ class _AttendScreenState extends State<AttendScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: TextButton(
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all(Colors.white),
-              backgroundColor: MaterialStateProperty.all(Color(0xFF2EB402)),
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Icon(
-              Icons.arrow_back,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: TextButton(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+            backgroundColor: MaterialStateProperty.all(Color(0xFF2EB402)),
           ),
-          title: Text(
-            'attendance status',
-            style: titleTextStyle,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back,
           ),
-          backgroundColor: Color(0xFF2EB402),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              ChallengeContainer(
-                  challengeName: 'running 20 Days',
-                  imagePath: 'images/Running.jpg'),
-              Container(
-                height: 450.0,
-                margin: EdgeInsets.all(16.0),
-                child: GridView.count(
-                  scrollDirection: Axis.vertical,
-                  crossAxisCount: 5,
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 5,
-                  children: List.generate(
-                    challengeDays,
-                    (index) {
-                      return StampCircle(
-                        onTap: checkList(index),
-                        index: index,
-                        iconList: iconList,
-                      );
-                    },
-                  ),
+        title: Text(
+          'attendance status',
+          style: titleTextStyle,
+        ),
+        backgroundColor: Color(0xFF2EB402),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ChallengeContainer(
+                challengeName: 'running 20 Days',
+                imagePath: 'images/Running.jpg'),
+            Container(
+              height: 450.0,
+              margin: EdgeInsets.all(16.0),
+              child: GridView.count(
+                scrollDirection: Axis.vertical,
+                crossAxisCount: 5,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 5,
+                children: List.generate(
+                  challengeDays,
+                  (index) {
+                    return StampCircle(
+                      onTap: checkList(index),
+                      index: index,
+                      iconList: iconList,
+                    );
+                  },
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
